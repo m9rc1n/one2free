ccc30195dc7033a1c643e66df02ce52cc963c35d
marc-inn <0.660162@gmail.com>
2016-01-17 22:12:01 +0100

    Fix problem with theme

--- app/src/main/java/com/nidelva/one2free/MainActivity.java
+++ app/src/main/java/com/nidelva/one2free/MainActivity.java
@@ -33,8 +33,11 @@ public class MainActivity extends AppCompatActivity
         });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
